19a1e07f66ce2b60b0ca2f0f090460b2674f9599
angelinaturchyn <75140309+angelinaturchyn@users.noreply.github.com>
2021-10-13 18:07:46 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const {sum,doubleDiv, addOne, mult,returnArr,sumOfnum,helloWorld,positive,power,percentV,quadraticEquation} = require('./Math')
+
+console.log(mult(5,2));
+console.log(sum( 5, 2));
+console.log(doubleDiv(4,8))
+console.log(helloWorld('Hello  World!' ));
+console.log(positive(-3));
+console.log(power(3,3));
+console.log(percentV(30,3));
+console.log(quadraticEquation(10,4,6));
+console.log(sumOfnum(2,5));
+console.log(returnArr(2,7));
+console.log(addOne([1, 2, -3, 4, -6, -7]));
+
